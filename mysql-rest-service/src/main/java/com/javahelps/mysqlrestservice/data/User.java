9e33e1e6eaf22720c086692a8c81498b4fac226f
Mudrika Trivedi <mudrikatrivedi20@gmail.com>
2020-06-11 23:02:09 +0530

    Integration: API configuration for backend and frontend
    Change-Id: I89e2efdd2ddb57a5cacca07977dbbbc1c7a7c371

---
+++ mysql-rest-service/src/main/java/com/javahelps/mysqlrestservice/data/User.java
@@ -0,0 +1,42 @@
+package com.javahelps.mysqlrestservice.data;
+import java.util.*;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "login")
+public class User {
+		@Id
+	    @GeneratedValue(strategy = GenerationType.AUTO)
+		@Column(name="EmpID")
+		private String empID;
+		@Column(name="Password")
+		private String password;
+			  public User(){
+				  
+			  }
+		public User(String empID, String password) {
+	        this.setEmpID(empID);
+	        this.setPassword(password);
+		
+		}
+		public void setEmpID(String empID) {
+			this.empID=empID;
+		}
+		public String getEmpID() {
+			return empID;
+		}
+
+		public void setPassword(String password) {
+			this.password=password;
+		}
+		public String getPassword() {
+			return password;
+		}
+		 @Override
+		    public String toString() {
+		        return "User{" +
+		                "EmpID=" + empID +
+		                ", password='" + password + '\'' +
+		                '}';
+		    }
+}
