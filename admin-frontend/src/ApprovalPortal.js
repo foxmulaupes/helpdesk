39a338887bc0c2fab26f092fce2a7b9be3dd0718
Kaustubh Asthana <kaustubhasthana2@gmail.com>
2020-06-14 14:40:47 +0530

    admin: approval feature for admin
    Signed-off-by: Kaustubh Asthana <kaustubhasthana2@gmail.com>
    Change-Id: Ib190a4c33868717451a83bc038a0ed55b0509a1c

---
+++ admin-frontend/src/ApprovalPortal.js
@@ -0,0 +1,170 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import AppBar from "./Appbar";
+//import FormControlLabel from '@material-ui/core/FormControlLabel';
+//import Checkbox from '@material-ui/core/Checkbox';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Link from '@material-ui/core/Link';
+import { makeStyles } from '@material-ui/core/styles';
+import ReactDOM from "react-dom";
+import AdminApproval from "./AdminApproval"
+
+function Render(){
+    return(
+    ReactDOM.render(<AdminApproval />, document.getElementById("root"))
+    );
+}
+
+
+
+function Copyright() {
+    return (
+      <Typography variant="body2" color="textSecondary" align="center">
+        {'Copyright © '}
+        <Link color="inherit" href="https://material-ui.com/">
+          Foxmula
+        </Link>{' '}
+        {new Date().getFullYear()}
+        {'.'}
+      </Typography>
+    );
+  }
+
+  const useStyles1 = makeStyles((theme) => ({
+    paper: {
+      marginTop: theme.spacing(8),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+      width: '100%', // Fix IE 11 issue.
+      marginTop: theme.spacing(1),
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 2),
+      
+    },
+  }));
+  
+  
+export default function ApprovalPortal() {
+const [value, setValue] = React.useState('Approve');
+const classes = useStyles1();
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+  return (
+    <form className={classes.form} noValidate>
+    <React.Fragment>
+      <Typography variant="h6" gutterBottom>
+        TicketDate
+      </Typography>
+      <Grid container spacing={5}>
+        <Grid item xs={12}>
+          <TextField
+            required
+            id="TicketDate"
+            //label="TicketDate"
+            //helperText="Tag Watchers to your Issue"
+            fullWidth
+          />
+        </Grid>
+      </Grid>
+      <Typography variant="h6" gutterBottom>
+        Ticket Title
+      </Typography>
+      <Grid container spacing={5}>
+        <Grid item xs={12}>
+          <TextField
+            required
+            id="TicketTitle"
+            //label="TicketTitle"
+            //helperText="Tag Watchers to your Issue"
+            fullWidth
+          />
+        </Grid>
+      </Grid>
+      <Typography variant="h6" gutterBottom>
+        Ticket Description
+      </Typography>
+      <Grid container spacing={5}>
+        <Grid item xs={12}>
+          <TextField
+            required
+            id="TicketDiscription"
+            //label="TicketDescription"
+            //helperText="Tag Watchers to your Issue"
+            fullWidth
+          />
+        </Grid>
+      </Grid>
+
+      <Typography variant="h6" gutterBottom>
+        Watchers
+      </Typography>
+      <Grid container spacing={5}>
+        <Grid item xs={12}>
+          <TextField
+            required
+            id="Watchers"
+            //label="Watchers Details"
+            //helperText="Tag Watchers to your Issue"
+            fullWidth
+          />
+        </Grid>
+      </Grid>
+      <Typography variant="h6" gutterBottom>
+        Priority
+      </Typography>
+      <Grid container spacing={5}>
+        <Grid item xs={12}>
+          <TextField
+            required
+            id="Priority"
+            //label="Priority"
+            //helperText="Tag Watchers to your Issue"
+            fullWidth
+          />
+        </Grid>
+      </Grid>
+      <Typography variant="h6" gutterBottom>
+        Approver Option
+      </Typography>
+      <Grid container spacing={5}>
+      <Grid item xs={12}>
+        <FormControl component="fieldset">
+        <RadioGroup aria-label="approveroption" name="approveroption" value={value} onChange={handleChange}>
+          <FormControlLabel value="Approve" control={<Radio />} label="Approve" />
+          <FormControlLabel value="Reject" control={<Radio />} label="Reject" />
+          <FormControlLabel value="Pending" control={<Radio />} label="Pending" />
+        </RadioGroup>
+      </FormControl>
+        </Grid>
+      </Grid>
+    </React.Fragment>
+    <Button
+            type="submit"
+            onClick={Render}
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Submit
+          </Button>
+
+        </form>
+        );
+}
